a403d8a2371dbaba0bc4bc37b8d2cae083eeb365
Steve Gardner <github@ste.vg>
2018-05-08 22:20:50 -0700

    ok changed from module export to plain class thing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,25 +1,20 @@
-declare module "Timeline" {
-    export enum LineSide {
-        right = 0,
-        left = 1,
-    }
-    export interface TimelineElement {
-        element: HTMLElement;
-        side: LineSide;
-        radius?: number[];
-    }
-    export interface Pos {
-        x: number;
-        y: number;
-    }
-    export class Timeline {
-        private options;
-        private svg;
-        private path;
-        constructor(svg: HTMLElement, options: TimelineElement[]);
-        draw(): void;
-    }
+declare enum LineSide {
+    right = 0,
+    left = 1,
 }
-declare module "index" {
-    export * from "Timeline";
+interface TimelineElement {
+    element: HTMLElement;
+    side: LineSide;
+    radius?: number[];
+}
+interface Pos {
+    x: number;
+    y: number;
+}
+declare class Timeline {
+    private options;
+    private svg;
+    private path;
+    constructor(svg: HTMLElement, options: TimelineElement[]);
+    draw(): void;
 }
